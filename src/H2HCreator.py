2680ef9bfd76c8a6f730d7c4fd4793722d59754b
JeremySkalla <77216636+JeremySkalla@users.noreply.github.com>
2022-12-27 14:51:45 -0600

    Create save_json option

--- src/H2HCreator.py
+++ src/H2HCreator.py
@@ -5,9 +5,10 @@ import players
 import results
 
 class H2HMaker(object):
-    def __init__(self, key, sleep_time=10): # Initializes object
+    def __init__(self, key, save_json:bool, sleep_time=10): # Initializes object
         self.key = key
         self.header = {"Authorization": "Bearer " + key}
+        self.save_json = save_json
         self.sleep_time = 10 # Sleep time is how long it waits between 6 queries to sleep
 
     def set_key(self, new_key): # Sets new key
@@ -16,6 +17,9 @@ class H2HMaker(object):
 
     def set_sleep_time(self, new_sleep_time):
         self.sleep_time = new_sleep_time
+
+    def set_save_json(self, new_save_json):
+        self.save_json = new_save_json
 
     def print_key(self):
         print(self.key)
