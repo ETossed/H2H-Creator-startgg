bed217b0fdd78cb8d64216ce8f81f65b477fac90
JeremySkalla <77216636+JeremySkalla@users.noreply.github.com>
2022-12-27 14:32:55 -0600

    Object basic setup

--- src/H2HCreator.py
+++ src/H2HCreator.py
@@ -1,2 +1,27 @@
 import requests
 
+class H2HMaker(object):
+    def __init__(self, key, sleep_time=10): # Initializes object
+        self.key = key
+        self.header = {"Authorization": "Bearer " + key}
+        self.sleep_time = 10 # Sleep time is how long it waits between 6 queries to sleep
+
+    def set_key(self, new_key): # Sets new key
+        self.key = new_key
+        self.header = {"Authorization": "Bearer " + new_key}
+
+    def set_sleep_time(self, new_sleep_time):
+        self.sleep_time = new_sleep_time
+
+    def print_key(self):
+        print(self.key)
+    
+    def print_header(self):
+        print(self.header)
+
+    def print_sleep_time(self):
+        print(self.sleep_time)
+
+def main():
+    # Testing area
+    return
